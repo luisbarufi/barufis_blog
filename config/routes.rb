cf87575b4cd42073b358e6d952056259435b937f
LuisBarufi <luisbarufi@gmail.com>
2019-11-27 12:37:20 -0300

    fixing home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   root 'home#index'
-  #resources :posts
+  resources :posts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
