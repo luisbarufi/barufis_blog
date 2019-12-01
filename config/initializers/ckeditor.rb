d445a38f5173ab36da1109bde9ba267600ff9084
LuisBarufi <luisbarufi@gmail.com>
2019-12-01 00:37:02 -0300

    added ckeditor

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -2,7 +2,6 @@
 
 # Use this hook to configure ckeditor
 Ckeditor.setup do |config|
-
   
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default), :mongo_mapper and
