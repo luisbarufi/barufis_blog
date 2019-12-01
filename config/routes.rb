1407d87d1d029705294ee1650810e22e9869071f
LuisBarufi <luisbarufi@gmail.com>
2019-12-01 08:38:43 -0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-Rails.application.routes.draw do
-
-  mount Ckeditor::Engine => '/ckeditor'
+Rails.application.routes.draw do  
   root 'home#index'
-
+  mount Ckeditor::Engine => '/ckeditor' 
   resources :posts
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
