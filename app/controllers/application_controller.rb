f5938856ec1acb92c261f49ba13f46a157e9a229
LuisBarufi <luisbarufi@gmail.com>
2019-12-01 22:37:53 -0300

    added setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-  
+  before_action :set_locale  
  
   def set_locale
     session[:locale] = params[:locale] if params[:locale].present?
