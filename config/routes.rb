13db76cd375548b53679d44d9c0c745282d31e40
LuisBarufi <luisbarufi@gmail.com>
2019-12-01 23:15:51 -0300

    added setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root 'home#index'
   
+  
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
