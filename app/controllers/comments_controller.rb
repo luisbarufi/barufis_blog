9f90320a6db3cffa357249c0efaa70c4e2ad70f6
luisbarufi <luisbarufi@gmail.com>
2019-12-22 21:12:39 -0300

    added comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+
+    if @comment.save
+      redirect_to @post
+    else
+      render 'posts/show'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy       
+    redirect_to post_path(@post)
+  end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_131726) do
+ActiveRecord::Schema.define(version: 2019_12_22_233635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2019_12_06_131726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -46,4 +55,5 @@ ActiveRecord::Schema.define(version: 2019_12_06_131726) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
