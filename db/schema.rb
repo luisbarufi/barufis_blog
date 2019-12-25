2353a685e27ece5caf9aca86d810d2d78b6a45ed
luisbarufi <luisbarufi@gmail.com>
2019-12-25 17:58:03 -0300

    fixed views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_233635) do
+ActiveRecord::Schema.define(version: 2019_12_25_151033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
