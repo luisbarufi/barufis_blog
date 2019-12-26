055d0347d575c64a309128312af4bb66f2410347
luisbarufi <luisbarufi@gmail.com>
2019-12-26 19:55:41 -0300

    fidex views

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   paginates_per 50
+  default_scope { order(created_at: :desc) }
   has_many :comments
 end
