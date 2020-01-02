2ef4288dac35ada5f046089b531fd97c712ec070
luisbarufi <luisbarufi@gmail.com>
2020-01-01 23:03:37 -0300

    fixed views

--- app/controllers/dislikes_controller.rb
+++ app/controllers/dislikes_controller.rb
@@ -3,33 +3,35 @@ class DislikesController < ApplicationController
   before_action :find_dislike, only: [:destroy]
 
   def create
-    if already_disliked?
-      flash[:notice] = "You can't like more than once"
-    else
-      @post.dislikes.create(user_id: current_user.id)
-    end
+    # if already_disliked?
+    #   flash[:notice] = "You can't like more than once"
+    # else
+    #   @post.dislikes.create(user_id: current_user.id)
+    # end
+    @post.dislikes.create(user_id: current_user.id)
     redirect_to post_path(@post)
   end
 
   def destroy
-    if !(already_disliked?)
-      flash[:notice] = "Cannot unlike"
-    else
-      @dislike.destroy
-    end
+    # if !(already_disliked?)
+    #   flash[:notice] = "Cannot unlike"
+    # else
+    #   @dislike.destroy
+    # end
+    @dislike.destroy
     redirect_to post_path(@post)
   end
 
 private
 
-  def already_disliked?
-    Dislike.where(user_id: current_user.id, post_id:
-    params[:post_id]).exists?
-  end
+  # def already_disliked?
+  #   Dislike.where(user_id: current_user.id, post_id:
+  #   params[:post_id]).exists?
+  # end
 
   def find_dislike
     @dislike = @post.dislikes.find(params[:id])
- end
+  end
 
   def find_post
     @post = Post.find(params[:post_id])
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,35 +3,36 @@ class LikesController < ApplicationController
   before_action :find_like, only: [:destroy]
 
   def create
-    if already_liked?
-      flash[:notice] = "You can't like more than once"
-    else
-      @post.likes.create(user_id: current_user.id)
-    end
+    # if already_liked?
+    #   flash[:notice] = "You can't like more than once"
+    # else
+    #   @post.likes.create(user_id: current_user.id)
+    # end
+    @post.likes.create(user_id: current_user.id)
     redirect_to post_path(@post)
   end
 
   def destroy
-    if !(already_liked?)
-      flash[:notice] = "Cannot unlike"
-    else
-      @like.destroy
-    end
+    # if !(already_liked?)
+    #   flash[:notice] = "Cannot unlike"
+    # else
+    #   @like.destroy
+    # end
+    @like.destroy
     redirect_to post_path(@post)
   end
 
- private
-
-  def already_liked?
-    Like.where(user_id: current_user.id, post_id:
-    params[:post_id]).exists?
-  end
-
+private
+  # def already_liked?
+  #   Like.where(user_id: current_user.id, post_id:
+  #   params[:post_id]).exists?
+  # end
+  
   def find_like
     @like = @post.likes.find(params[:id])
   end
 
- def find_post
-   @post = Post.find(params[:post_id])
- end
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 end
